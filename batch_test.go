4be532dd3535e3256d4112e4c35e5a0ed764fc2e
Derich Pacheco <carlosderich@gmail.com>
2025-05-09 12:19:13 -0300

    feat: idempotency key support for batch send

--- batch_test.go
+++ batch_test.go
@@ -1,6 +1,7 @@
 package resend
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 	"testing"
@@ -40,6 +41,49 @@ func TestBatchSendEmail(t *testing.T) {
 	resp, err := client.Batch.Send(req)
 	if err != nil {
 		t.Errorf("BatchEmail.Send returned error: %v", err)
+	}
+	assert.Equal(t, resp.Data[0].Id, "1")
+	assert.Equal(t, resp.Data[1].Id, "2")
+}
+
+func TestBatchSendWithOptionsEmail(t *testing.T) {
+	setup()
+	defer teardown()
+	ctx := context.Background()
+
+	mux.HandleFunc("/emails/batch", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPost)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		ret := &BatchEmailResponse{
+			Data: []SendEmailResponse{
+				{Id: "1"},
+				{Id: "2"},
+			},
+		}
+		err := json.NewEncoder(w).Encode(&ret)
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	req := []*SendEmailRequest{
+		{
+			To: []string{"d@e.com"},
+		},
+		{
+			To: []string{"d@e.com"},
+		},
+	}
+
+	options := &BatchSendEmailOptions{
+		IdempotencyKey: "1234567890",
+	}
+
+	resp, err := client.Batch.SendWithOptions(ctx, req, options)
+	if err != nil {
+		t.Errorf("BatchEmail.SendWithOptions returned error: %v", err)
 	}
 	assert.Equal(t, resp.Data[0].Id, "1")
 	assert.Equal(t, resp.Data[1].Id, "2")
