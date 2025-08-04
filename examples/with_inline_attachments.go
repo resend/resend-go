6325dfcd1b7b1aadfefbb63ef4d6b751cbe2c3a9
Derich Pacheco <carlosderich@gmail.com>
2025-08-03 22:37:23 -0300

    fix: use delivered email addr

--- examples/with_inline_attachments.go
+++ examples/with_inline_attachments.go
@@ -26,7 +26,7 @@ func inlineAttachmentExample() {
 	}
 
 	params := &resend.SendEmailRequest{
-		To:          []string{"carlosderich@gmail.com"},
+		To:          []string{"delivered@resend.dev"},
 		From:        "onboarding@resend.dev",
 		Text:        "email with inline content attachment",
 		Html:        "<p>This is an email with an <img width=100 height=40 src=\"cid:my-test-image\" /> embed image</p>",
